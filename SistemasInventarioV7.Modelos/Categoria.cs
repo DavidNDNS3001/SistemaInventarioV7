3124e6132fdc61e6ad5987f39b08a7b0612a92e0
DavidNDNS3001 <ndns.david@gmail.com>
2024-08-06 14:34:08 -0500

    CRUD De categoria

---
+++ SistemasInventarioV7.Modelos/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasInventarioV7.Modelos
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nombre de categoria es requerido")]
+        [MaxLength(100, ErrorMessage ="Nombre de categoria debe ser maximo 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion de categoria es requerido")]
+        [MaxLength(100, ErrorMessage = "Descripcion de categoria debe ser maximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Estado de categoria es requerido")]
+        public bool Estado { get; set; }
+
+    }
+}
